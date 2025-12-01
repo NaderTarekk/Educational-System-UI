eb3da7235899fb102f951969be8e1e0e6e6c7c39
NaderTarekk <tareknader1000@yahoo.com>
2025-12-01 23:05:40 +0200

    initial

--- src/app/shared/components/sidenav/sidenav.ts
+++ src/app/shared/components/sidenav/sidenav.ts
@@ -1,4 +1,4 @@
-import { ChangeDetectorRef, Component, OnInit } from '@angular/core';
+import { ChangeDetectorRef, Component, HostListener, OnInit } from '@angular/core';
 import { SharedService } from '../../services/shared';
 import { BehaviorSubject } from 'rxjs';
 
@@ -10,13 +10,20 @@ import { BehaviorSubject } from 'rxjs';
 })
 export class Sidenav implements OnInit {
   role: string | null = null;
-
+ isMobile: boolean = false;
   constructor(public sidenavService: SharedService) { }
 
   ngOnInit(): void {
     this.role = localStorage.getItem("NHC_PL_Role");
+      this.checkScreenSize();
   }
-
+  onResize() {
+    this.checkScreenSize();
+  }
+  @HostListener('window:resize')
+  checkScreenSize() {
+    this.isMobile = window.innerWidth < 1024;
+  }
   isDropdownOpen = false;
 
   toggleDropdown() {
